ca8938477149b5ef394f9be600c4227f1520d57e
Teagar <cyanofc@gmail.com>
2023-11-16 18:52:01 -0300

    java 01, 02

---
+++ exercicio-01/app.java
@@ -0,0 +1,49 @@
+public class Exercicio01 {
+
+  public static void main(String[] args) {
+
+  /*
+  01 - Declare uma const "myBirthYear", que recebe o ano do seu nascimento, com 4 dígitos. Ex.: 1930
+  */
+
+
+
+  /*
+  02 - Declare uma const "currentYear", que recebe o ano atual, com 4 dígitos.
+  */
+
+
+  
+  /*
+  03 - Exiba as const "myBirthYear" e "currentYear", lado a lado, no console do browser.
+  */
+
+
+
+  /*
+  04 - Experimente.
+
+    - Declare uma const "myAge", que recebe o ano atual subtraído pelo ano do seu nascimento. Mas sem digitar os números manualmente. 
+  * Você pode utilizar o sinal de menos (-) entre os dois valores e verificar o que
+  * acontece.
+  * 
+  * - Exiba a "myAge" no console.
+  */
+
+  /*
+  09 - Antes do console.log abaixo, declare uma const "ten", que recebe o número 10 e:
+   
+   - Descomente o console.log;
+   - Substitua a palavra "valor1" pelo ano atual somado à 10;
+   - Substitua a palavra "valor2" pela sua idade somada à 10;
+   - Os números não devem ser digitados manualmente;
+   - Observe o resultado no console do browser.
+   
+   Não se assuste com as crases e os cifrões. Conversaremos mais sobre operações matemáticas e strings (textos) em breve =)
+  */
+
+  // System.out.println("Em " + valor1 + ", estarei com " + valor2 + " anos.");
+
+  }
+}
+
---
+++ exercicio-02/app.java
@@ -0,0 +1,219 @@
+public class Exercicio02 {
+
+  public static void main(String[] args) {
+
+  /*
+  01 - No index.html deste diretório (exercicio-02):
+
+  - Declare uma marcação HTML;
+  - Modifique a linguagem para português do Brasil;
+  - Remova as meta tags desnecessárias;
+  - Modifique o title para "Exercício 02";
+  - E linke este arquivo, app.js, antes do fechamento da tag body.
+  */
+
+
+
+  /*
+  02 - Inicie o seu servidor local e abra o console do browser.
+  */
+
+
+
+  /*
+  03 - Exiba a string 'Grandes poderes trazem grandes responsabilidades.' no console do browser.
+
+  Utilize os espaços entre os comentários abaixo para executar os códigos dos exercícios. Inclusive este.
+  */
+
+
+
+  /*
+  04 - Comente o System.out.println acima, e:
+
+  - Armazene a palavra 'Tom' em uma constante "tom";
+  - Armazene a palavra 'Hanks' em uma constante "firstTom";
+  - Armazene a palavra 'Cruise' em uma constante "secondTom";
+  - Declare uma constante "myFavoriteTom" que recebe nome, espaço, e sobrenome do Tom que você mais gosta;
+  - Exiba a "myFavoriteTom" no console do browser.
+  */
+
+
+
+  /*
+  05 - Comente o System.out.println acima, e:
+
+  - Armazene a palavra 'Java Language' em uma constante "programmingLanguage";
+  - Armazene a junção das letras "J" e "L", da programmingLanguage, em uma constante "JL";
+  - As letras não devem ser digitadas manualmente;
+  - Exiba a "JL" no console do browser;
+  */
+
+
+  /*
+  06 - Comente o console.log acima, e,
+
+  - Armazene, em uma constante "jdk", a junção das letras iniciais das 3 palavras que a constante "oracle" armazena;
+  - As iniciais não devem ser digitadas manualmente;
+  - Exiba a "jdk" no console do browser;
+  */
+
+  final String oracle = "Java Development Kit";
+
+  /*
+  07 - Comente o System.out.println acima, e,
+
+  - Exiba no console a quantidade de caracteres que a constante "programmingLanguage" possui;
+  */
+
+
+
+  /*
+  08 - Comente o System.out.println acima, e,
+
+  - Exiba no console a última letra da string que a "randomWord" (abaixo) armazena, mas com a condição a seguir;
+  - Ao invés de inserir manualmente a posição do último caractere, faça com que a obtenção da última letra funcione para qualquer palavra que a "randomWord" receber;
+  - Dica: utilize a propriedade ".length".
+  */
+
+  String randomWord = "Araponga";
+
+  /*
+  09 - Comente o System.out.println acima, e,
+
+  - Exiba, no console, a string que "summerMessage" (abaixo) armazena, mas com todas as letras minúsculas;
+  - Não modifique a string da "summerMessage";
+  - Não digite a nova string manualmente.
+  */
+
+  final String summerMessage = "O VERÃO CHEGA NESTE FIM DE SEMANA";
+
+  /*
+  10 - Comente o System.out.println acima, e,
+
+  - Exiba, no console, a string que "winterMessage" (abaixo) armazena, mas com todas as letras maiúsculas;
+  - Não modifique a string da "winterMessage";
+  - Não digite a nova string manualmente.
+  */
+
+  final String winterMessage = "o inverno está chegando";
+
+  /*
+  11 - Comente o System.out.println acima, e,
+
+  - Exiba, no console, o index do caractere "v" da "winterMessage";
+  */
+
+
+
+  /*
+  12 - Comente o System.out.println acima, e,
+
+  - Exiba, no console, o index da última ocorrência do caractere "n" da "winterMessage".
+  */
+
+
+
+  /*
+  13 - Comente o System.out.println acima, e,
+
+  - Após a const "harryPotterAuthor" (abaixo), exiba no console a parte "J.K." da string "harryPotterAuthor";
+  - Não digite a string "J.K." manualmente;
+  */
+
+  final String harryPotterAuthor = "J.K. Rowling";
+
+  /*
+  14 - Comente o System.out.println acima, e,
+
+  - Após a const "bestMovie" (abaixo), declare uma constante "realBestMovie";
+  - Atribua à "realBestMovie" a mesma string da "bestMovie", mas substituindo o nome do filme para "Moonlight";
+  - Exiba a "realBestMovie" no console.
+  */
+
+  final String bestMovie = "E o Oscar de melhor filme vai para: 'La la land'!";
+
+  /*
+  15 - Comente o System.out.println acima, e,
+
+  - Abaixo deste comentário, armazene o resto de 10 dividido por 3 em uma constante "reminder";
+  - Exiba a reminder no console. O resultado deve ser 1;
+  */
+
+
+
+  /*
+  16 - Comente o System.out.println acima, e,
+
+  - Abaixo do int "episodes", incremente o valor dela em 1;
+  - Após o incremento, exiba o int "episodes" no console;
+  - O valor exibido no console deve ser 7.
+  */
+
+  int episodes = 6;
+
+  /*
+  17 - Comente o System.out.println acima, e,
+
+  - Decremente o int "episodes" em 1;
+  - Após o decremento, exiba o int "episodes" no console;
+  - O valor exibido no console deve ser 6.
+  */
+
+
+
+  /*
+  18 - Comente o System.out.println acima, e,
+
+  - Utilize o operador addition assignment para atualizar o valor da int "episodes" para 10;
+  - Após a atribuição, exiba o int "episodes" no console.
+  */
+
+
+
+  /*
+  19 - Comente o System.out.println acima, e,
+
+  - Abaixo deste comentário, armazene a string 'O nº de episódios é: NUMERO_DE_EPISÓDIOS' em uma constante "allEpisodes";
+  - Insira o int "episodes" no lugar de "NUMERO_DE_EPISÓDIOS";
+  - Exiba a "allEpisodes" no console.
+  */
+
+
+
+  /*
+  20 - Comente o System.out.println acima, e,
+
+  - Descomente a final "bookMessage" e o System.out.println abaixo;
+  - Encontre uma forma de incluir as aspas simples do nome do livro na string;
+  - Não modifique nenhuma aspas da string para aspas duplas.
+  - Não use template strings.
+  */
+
+  // const bookMessage = "'O Conto da Aia' é um dos livros mais vendidos da década.";
+  // System.out.println(bookMessage);
+
+  /*
+  21 - Comente o System.out.println acima (e a const bookMessage caso não tenha descoberto a resolução), e,
+
+  - Após a final "name" (abaixo), declare uma let "newName";
+  - O int "newName" deverá armazenar uma string ES5 com o mesmo valor que "name" armazena;
+  - Só que com as letras iniciais do nome e sobrenome maiúsculas;
+  - Não digite a nova string manualmente.
+  - Não modifique a string da final "name";
+  - Exiba a "newName" no console;
+  */
+
+  final String name = "walter white";
+
+  /*
+  22 - Comente o System.out.println acima, e,
+
+  - Atribua à "newName" uma template string;
+  - A template string deverá resultar no mesmo valor que a "newName" tinha anteriormente (nome e sobrenome com letras iniciais maiúsculas);
+  - Não digite a nova string manualmente.
+  - Não modifique a string da final "name";
+  - Exiba a "newName" no console;
+  */
+  }
+}
